58020f3d8d699752c21015fddfc144229f90029a
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-04 02:50:29 +0530

    Additions done to optimize code

---
+++ IAM_constants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 23:19:38 2020
+
+@author: Tanmay Thakur
+"""
+
+class constants:
+    kernelSize = 25
+    sigma = 11
+    theta = 7
+    minArea = 100
+    height = 300
